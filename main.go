61620265c6a839412dfbda8dfc873b0738fd0ab2
dilan <125643732+therealdilan@users.noreply.github.com>
2023-10-25 11:56:24 +0400

    Working Task State
    tasks will be strikethroughed if done

--- main.go
+++ main.go
@@ -1,27 +1,35 @@
-package main 
+package main
 
 import (
 	"fmt"
-	"log"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 type task struct {
-	Title string
+	State   bool
 	Content string
 }
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Server Working")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		htmlTemplate := template.Must(template.ParseFiles("index.html"))
 		htmlTemplate.Execute(w, nil)
+
+		todoList := map[string][]task{
+			"Tasks": {
+				{Content: "Some content", State: true},
+				{Content: "Some more content", State: false},
+			},
+		}
+
+		htmlTemplate.Execute(w, todoList)
 	}
 
 	http.HandleFunc("/", h1)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
