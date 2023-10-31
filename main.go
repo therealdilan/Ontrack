f021587fdd6f982449413e3c54f189cc101cdc53
dilan <125643732+therealdilan@users.noreply.github.com>
2023-10-31 22:05:29 +0400

    playing around with templates

--- main.go
+++ main.go
@@ -10,35 +10,35 @@ import (
 type task struct {
 	State   bool
 	Content string
+	ID      int
 }
 
 func main() {
+	var todoList map[string]int
+
 	fmt.Println("Server Working")
 
-	pageLoad := func(w http.ResponseWriter, r *http.Request) {
-		htmlTemplate := template.Must(template.ParseFiles("index.html"))
-
-		todoList := map[string][]task{
-			"Tasks": {
-				{Content: "Some content", State: true},
-				{Content: "Some more content", State: false},
-			},
-		}
-
-		htmlTemplate.Execute(w, todoList)
-	}
+	todoList := make(map[string]task{
+		"Tasks": {
+			{Content: "Some content", State: false, ID: 1},
+		},
+	})
 
 	addTask := func(w http.ResponseWriter, r *http.Request) {
 		newTask := r.PostFormValue("newTask")
-		newTaskHTML := fmt.Sprintf("<div class='m-4 mt-4'><p class='text-2xl'>%s</p><button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Done</button><button class='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Remove</button></div>", newTask)
-		htmlTemplate, _ := template.New("addNewTask").Parse(newTaskHTML)
-		htmlTemplate.Execute(w, nil)
+		htmlTemplate := template.Must(template.ParseFiles("index.html"))
+		htmlTemplate.ExecuteTemplate(w, "taskItem", task{Content: newTask, State: false, ID: setID()})
+
 	}
 
 	http.HandleFunc("/", pageLoad)
 	http.HandleFunc("/add-task", addTask)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setID() int {
+	return len(todoList) + 1
 }
 
 //go run ./main.go
