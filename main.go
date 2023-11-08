4d6a67ff81354862a20ed1822a9e6e1dd5045170
dilan <125643732+therealdilan@users.noreply.github.com>
2023-11-08 22:22:57 +0400

    Tasks can now be removed from the list and dom

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type task struct {
 	State   bool
 	Content string
 	ID      int
+}
+
+func (t task) String() string { // For Debugging, will properly display the formatted task struct
+	return fmt.Sprintf("ID: %d, Content: %s", t.ID, t.Content)
 }
 
 var taskID int = 0
@@ -20,6 +25,7 @@ func main() {
 
 	http.HandleFunc("/", pageLoad)
 	http.HandleFunc("/add-task", addTask)
+	http.HandleFunc("/remove-task", removeTask)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 	print("Server is running on port 8000")
@@ -36,11 +42,32 @@ func addTask(w http.ResponseWriter, r *http.Request) {
 		State:   false,
 		ID:      taskID,
 	}
-
 	tasks = append(tasks, newTask)
 	taskID++
 
 	htmlTemplate := template.Must(template.ParseFiles("index.html"))
 	htmlTemplate.ExecuteTemplate(w, "taskItem", newTask)
+}
+
+func removeTask(w http.ResponseWriter, r *http.Request) {
+	idParam := r.URL.Query().Get("id") // Get the ID from the URL query parameter
+	fmt.Println(idParam)
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	removeTaskByID(id)
 	fmt.Println(tasks)
+}
+
+func removeTaskByID(id int) {
+	for i, t := range tasks {
+		if t.ID == id {
+			// Remove the task from the slice by slicing it
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			return
+		}
+	}
 }
