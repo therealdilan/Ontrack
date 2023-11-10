a566f3e59315ad8b0186421bd99a02847c2b6c53
dilan <125643732+therealdilan@users.noreply.github.com>
2023-11-10 20:28:39 +0400

    added comments

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type task struct {
 	ID      int
 }
 
-func (t task) String() string {
+func (t task) String() string { // For Debugging
 	return fmt.Sprintf("ID: %d, Content: %s, State: %t", t.ID, t.Content, t.State)
 }
 
@@ -23,38 +23,41 @@ var tasks []task
 
 func main() {
 
+	// Every HTTP request handling
+
 	http.HandleFunc("/", pageLoad)
 	http.HandleFunc("/add-task", addTask)
 	http.HandleFunc("/remove-task", removeTask)
 	http.HandleFunc("/mark-task", markTask)
 
+	// Start the server
+
 	log.Fatal(http.ListenAndServe(":8000", nil))
 	print("Server is running on port 8000")
 }
 
-func pageLoad(w http.ResponseWriter, r *http.Request) {
+func pageLoad(w http.ResponseWriter, r *http.Request) { // Load the main page
 	htmlTemplate := template.Must(template.ParseFiles("index.html"))
 	htmlTemplate.Execute(w, nil)
 }
 
-func addTask(w http.ResponseWriter, r *http.Request) {
+func addTask(w http.ResponseWriter, r *http.Request) { // Adding a new Task
 	newTask := task{
 		Content: r.FormValue("newTask"),
 		State:   false,
 		ID:      taskID,
 	}
-	tasks = append(tasks, newTask)
-	taskID++
+	tasks = append(tasks, newTask) // Append the new task to the array
+	taskID++                       // Increment the ID to be used for the new added task
 
 	htmlTemplate := template.Must(template.ParseFiles("index.html"))
 	htmlTemplate.ExecuteTemplate(w, "taskItem", newTask)
-	fmt.Println(tasks)
 }
 
 func removeTask(w http.ResponseWriter, r *http.Request) {
 	idParam := r.URL.Query().Get("id") // Get the ID from the URL query parameter
 	fmt.Println(idParam)
-	id, err := strconv.Atoi(idParam)
+	id, err := strconv.Atoi(idParam) // Convert the ID to an integer
 	if err != nil {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
@@ -64,10 +67,9 @@ func removeTask(w http.ResponseWriter, r *http.Request) {
 }
 
 func removeTaskByID(id int) {
-	for i, t := range tasks {
+	for i, t := range tasks { // Find the task with the given ID
 		if t.ID == id {
-			// Remove the task from the slice by slicing it
-			tasks = append(tasks[:i], tasks[i+1:]...)
+			tasks = append(tasks[:i], tasks[i+1:]...) // Removing the task
 			return
 		}
 	}
@@ -77,19 +79,18 @@ func removeTaskByID(id int) {
 func markTask(w http.ResponseWriter, r *http.Request) {
 	idParam := r.URL.Query().Get("id") // Get the ID from the URL query parameter
 	fmt.Println(idParam)
-	id, err := strconv.Atoi(idParam)
+	id, err := strconv.Atoi(idParam) // Convert the ID to an integer
 	if err != nil {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
 
-	markTaskasDone(id, w)
-	fmt.Println(tasks)
+	markTaskasDone(id, w) // Sending the new HTML with the changed task
 }
 
 func markTaskasDone(id int, w http.ResponseWriter) {
 	var taskToUpdate *task
-	for i := range tasks {
+	for i := range tasks { // Find the task with the given ID
 		if tasks[i].ID == id {
 			taskToUpdate = &tasks[i]
 			break
@@ -99,8 +100,9 @@ func markTaskasDone(id int, w http.ResponseWriter) {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
+
 	taskToUpdate.State = !taskToUpdate.State
 	htmlTemplate := template.Must(template.ParseFiles("index.html"))
-	htmlTemplate.ExecuteTemplate(w, "taskItem", taskToUpdate)
-	fmt.Println(tasks)
+
+	htmlTemplate.ExecuteTemplate(w, "taskItem", taskToUpdate) // Once you get the task, send the task (with new State)
 }
