b88681780e650b592adda0948241c0bd0a624e7d
dilan <125643732+therealdilan@users.noreply.github.com>
2023-12-04 00:29:04 +0400

    firebase set up

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"net/http"
+  "fmt"
+  "context"
+  "net/http"
 
-	"github.com/therealdilan/ontrack/app"
+  firebase "firebase.google.com/go"
+
+  "google.golang.org/api/option"
+  "github.com/therealdilan/ontrack/app"
 )
 
 func main() {
-	// Register the static route outside HandleTemplate
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("app/static"))))
+  opt := option.WithCredentialsFile("admin.firebase.json")
+  app, err := firebase.NewApp(context.Background(), nil, opt)
+  
+  fmt.Println("backend: ", app)
 
-	http.HandleFunc("/", app.HandleTemplate("index.html"))  
+  if err != nil {
+    fmt.Errorf("error initializing app: ", err)
+  } // Register the static route outside HandleTemplate
+	
+  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("app/static"))))
+	http.HandleFunc("/", handler.HandleTemplate("index.html"))
   http.ListenAndServe(":8080",nil)
 }
 
+func consoleLog(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("working :3")
+  w.Write([]byte("working :3")) 
+}
